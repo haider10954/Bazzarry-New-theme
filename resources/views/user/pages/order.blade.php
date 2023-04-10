469900a7116b7aedcfbadf1795555984687fc3ae
Haider Zaman <chhaider10954@gmail.com>
2023-04-10 15:57:08 +0500

    Minor UI changes

--- resources/views/user/pages/order.blade.php
+++ resources/views/user/pages/order.blade.php
@@ -97,43 +97,6 @@
             </div>
             <!-- End of Order Details -->
 
-            <div class="sub-orders mb-10">
-                <h4 class="title mb-5 font-weight-bold ls-25">Sub Orders</h4>
-                <div class="alert alert-icon alert-inline mb-5">
-                    <i class="w-icon-exclamation-triangle"></i>
-                    <strong>Note: </strong>This order has products from multiple vendors. So we divided this order into multiple vendor orders.
-                    Each order will be handled by their respective vendor independently.
-                </div>
-                <table class="order-subtable">
-                    <thead>
-                        <tr>
-                            <th class="order">Order</th>
-                            <th class="date">Date</th>
-                            <th class="status">Status</th>
-                            <th class="total">Total</th>
-                            <th class="action"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td class="order">950</td>
-                            <td class="date">April 23, 2021</td>
-                            <td class="status">On hold</td>
-                            <td class="total">$40.00 for 1 items</td>
-                            <td class="action"><a href="order-view.html" class="btn btn-rounded">View</a></td>
-                        </tr>
-                        <tr>
-                            <td class="order">951</td>
-                            <td class="date">April 25, 2021</td>
-                            <td class="status">On hold</td>
-                            <td class="total">$60.00 for 1 items</td>
-                            <td class="action"><a href="order-view.html" class="btn btn-rounded">View</a></td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-            <!-- End of Sub Orders-->
-
             <div id="account-addresses">
                 <div class="row">
                     <div class="col-sm-6 mb-8">
